c9ab67529b097bb3eb951af90d6b31c54e93848e
Raffy Quinto <raffyquinto@gmail.com>
2012-06-25 21:16:37 -0700

    I commit! 2

--- Test1.java
+++ Test1.java
@@ -1,6 +1,6 @@
 public class Test1{
 	public static void main(String[] args){
-		SmartSMS messaging = new SmartSMS ("001259","0012592000001346","Fu9wPaknmbsIx2kk+kAMyX0I/7s=","2010-08-21T08:33:46Z","2010082108334600001","406821");
+		SmartSMS messaging = new SmartSMS ("001259","0012592000001346","Fu9wPaknmbsIx2kk+kAMyX0I/7s=a","2010-08-21T08:33:46Z","2010082108334600001","406821");
 		
 		messaging.setRecipient("639177192127");
 		messaging.setMessage("Hello Moto!");
